05418e1696a963ac8efeaacf731026f3022d2c80
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-04-17 19:45:02 +0200

    crud utente

--- FINAL_PROJECT_CAPSTONE_SERVER/Controllers/UtenteController.cs
+++ FINAL_PROJECT_CAPSTONE_SERVER/Controllers/UtenteController.cs
@@ -1,10 +1,14 @@
 ﻿using FINAL_PROJECT_CAPSTONE_SERVER.Data;
+using FINAL_PROJECT_CAPSTONE_SERVER.Models;
+using FINAL_PROJECT_CAPSTONE_SERVER.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.IdentityModel.Tokens.Jwt;
 
 namespace FINAL_PROJECT_CAPSTONE_SERVER.Controllers
 {
-
+	[Authorize]
 	[Route("[controller]")]
 	[ApiController]
 	public class UtenteController : ControllerBase
@@ -70,6 +74,96 @@ namespace FINAL_PROJECT_CAPSTONE_SERVER.Controllers
 			}
 
 			return BadRequest(new { messaggio = "qualcosa è andato storto" });
+		}
+
+
+		[HttpPost("CheckPassword")]
+		public async Task<IActionResult> checkPasswordUtente([FromBody] string password)
+		{
+			var IdUtenteLoggato = User.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+
+			if (IdUtenteLoggato != null)
+			{
+				var UtenteLoggato = await _db.Utenti.Where(t => t.IdUtente == Convert.ToInt32(IdUtenteLoggato)).FirstOrDefaultAsync();
+
+				if (UtenteLoggato != null)
+				{
+					if (UtenteLoggato.Password == password)
+					{
+						return Ok(new { message = "Password Corrispondenti." });
+					}
+					return BadRequest(new { message = "Password non Corrispondenti." });
+				}
+				return BadRequest(new { message = "utente non trovato." });
+			}
+
+			return BadRequest(new { message = "Errore. idUtente non trovato nel server " });
+		}
+
+
+		[HttpPost("modificaDatiUtente/{idUtente}")]
+		public async Task<IActionResult> modificaDati([FromBody] ModificaDatiUtenteDTO datiUtente, [FromRoute] int idUtente)
+		{
+			if (ModelState.IsValid)
+			{
+
+				var IdUtenteLoggato = User.FindFirst(JwtRegisteredClaimNames.Jti)?.Value;
+
+				if (Convert.ToInt32(IdUtenteLoggato) == idUtente)
+				{
+					var UtenteLoggato = await _db.Utenti.Where(t => t.IdUtente == idUtente).FirstOrDefaultAsync();
+
+					if (UtenteLoggato != null)
+					{
+
+						if (datiUtente.Nome != null)
+						{
+							if (datiUtente.Nome == "")
+							{
+								UtenteLoggato.Nome = UtenteLoggato.Nome;
+							}
+							else
+							{
+								UtenteLoggato.Nome = datiUtente.Nome;
+							}
+						}
+
+						if (datiUtente.Cognome != null)
+						{
+							if (datiUtente.Cognome == "")
+							{
+								UtenteLoggato.Cognome = UtenteLoggato.Cognome;
+							}
+							else
+							{
+								UtenteLoggato.Cognome = datiUtente.Cognome;
+							}
+
+
+						}
+
+						if (datiUtente.Email != null)
+						{
+							if (datiUtente.Email == "")
+							{
+								UtenteLoggato.Email = UtenteLoggato.Email;
+							}
+							else
+							{
+								UtenteLoggato.Email = datiUtente.Email;
+							}
+
+
+						}
+
+
+						_db.Update(UtenteLoggato);
+						await _db.SaveChangesAsync();
+						return Ok(new { message = "Modifiche Effettuate", UtenteLoggato });
+					}
+				}
+			}
+			return BadRequest();
 		}
 	}
 }
---
+++ FINAL_PROJECT_CAPSTONE_SERVER/ViewModel/ModificaDatiUtenteDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FINAL_PROJECT_CAPSTONE_SERVER.ViewModel
+{
+	public class ModificaDatiUtenteDTO
+	{
+
+		public string? Nome { get; set; }
+		public string? Cognome { get; set; }
+		public string? Email { get; set; }
+	}
+}
